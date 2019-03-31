f96c918b7bbd33de10dd0c36a65b6f632dc890a7
Paul Greenham <paul.greenham@gmail.com>
2019-03-31 13:40:02 +0300

    set up node lesson files

---
+++ node/shapesApp.js
@@ -0,0 +1,7 @@
+const circleUtilities = require('./circleUtils')
+
+const r = 4
+console.log(`The area of a circle with radius ${r} is ${circleUtilities.calcCircleArea(r)}`)
+//^prints "The area of a circle with radius 4 is 50.26544"
+
+console.log(title)
